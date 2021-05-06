1c24c2f7ea3caf8939585da99bbe70994fb2b99e
Rafael447 <camposrafael28@gmail.com>
2021-05-06 00:58:50 -0300

    project created

---
+++ src/test/java/com/aplicacaoweb/demo/AplicacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacaoweb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
